0a1ec2c3a2ca9d18f9b886aca2c1ca1dd33b2b41
Hanna Lee <dlgkssk707@gmail.com>
2021-09-23 13:40:06 +0900

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.18

---
+++ src/ApproximateDPs.jl
@@ -0,0 +1,5 @@
+module ApproximateDPs
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ApproximateDPs
+using Test
+
+@testset "ApproximateDPs.jl" begin
+    # Write your tests here.
+end
